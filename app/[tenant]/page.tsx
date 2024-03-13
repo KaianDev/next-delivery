b98409cef7e2169a6aa7eae8201ae59d4e8216f3
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-13 13:49:53 -0300

    feat: get tenant in Home Page

--- app/[tenant]/page.tsx
+++ app/[tenant]/page.tsx
@@ -1,9 +1,30 @@
 // Components
+import { Metadata } from "next"
 import { Banner } from "./_components/banner"
 import { Header } from "./_components/header"
 import { ProductGrid } from "./_components/product-grid"
+import { useApi } from "@/lib/api"
+import { notFound } from "next/navigation"
 
-const HomePage = () => {
+interface HomePageProps {
+  params: {
+    tenant: string
+  }
+}
+
+export const generateMetadata = ({ params }: HomePageProps): Metadata => {
+  const api = useApi()
+  const tenant = api.getTenant(params.tenant)
+  return {
+    title: tenant ? `Next Delivery | ${tenant.name}` : "Página não encontrada",
+  }
+}
+
+const HomePage = async ({ params }: HomePageProps) => {
+  const api = useApi()
+  const tenant = api.getTenant(params.tenant)
+  if (!tenant) return notFound()
+
   return (
     <>
       <Header />
