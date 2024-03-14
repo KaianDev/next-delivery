909443de4037985271111f225cfe690a9381214e
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-14 17:18:37 -0300

    feat: add Custom button component

---
+++ components/custom-button.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { ComponentProps } from "react"
+
+// Components
+import { Button } from "./ui/button"
+
+// Utilities
+import { cn } from "@/lib/utils"
+
+interface CustomButtonProps extends ComponentProps<"button"> {
+  inverter?: boolean
+  mainColor?: string
+}
+
+export const CustomButton = ({
+  children,
+  className,
+  inverter,
+  ...rest
+}: CustomButtonProps) => {
+  return (
+    <button
+      className={cn(
+        "h-14 w-full rounded border-2 border-tenant bg-tenant text-base font-semibold text-white ease-out hover:bg-tenant/80",
+        inverter && "bg-transparent text-tenant hover:bg-transparent",
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
